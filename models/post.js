05573530dd0886b081329bf94cd0576155335d31
Jon Kesler <jkesler@hotmail.com>
2020-07-11 14:10:38 -0400

    database schema and seeds-working on api-routes

---
+++ models/post.js
@@ -0,0 +1,26 @@
+// Needs updating********************************
+
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        defaultValue: "Personal"
+      }
+    });
+    return Post;
+  };
+  
