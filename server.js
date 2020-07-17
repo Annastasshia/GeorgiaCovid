15e9b8c1fb3028d1b02b5cec85c272c91cb08a15
Annastasshia Ames <aames26@gmail.com>
2020-07-16 20:33:55 -0400

    added url route from map to counties page, correctly initailized handlebars, terminated html files, etc

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 var express = require("express");
 var session = require("express-session");
 
-
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -14,16 +13,48 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("public"));
+//Sets handlebars configurations 
+app.engine("handlebars", handlebars({
+  defaultLayout: "main",
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+
+//Sets our app to use the handlebars engine
+
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render("index");
+});
+
+app.get('/counties/:countyName', (req, res) => {
+  db.Coviddata.findOne({where: {cName: req.params.countyName}})
+    .then(function(dbData){
+      res.render("county", dbData.dataValues);
+    })
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  
+});
+
 
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
+
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -32,3 +63,4 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
